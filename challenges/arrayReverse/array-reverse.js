5d0202c1d5da5a1ed6280081caa914557b48fe53
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-09-07 01:18:51 +0300

    array revers

---
+++ challenges/arrayReverse/array-reverse.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function reverseArray(arr) {
+  let reversedArr = [];
+  let reverseIndex = 0;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reversedArr[reverseIndex] = arr[i];
+    reverseIndex++;
+  }
+  return reversedArr;
+}
+
+//test the function
+let arr = [1, 2, 3, 4, 5, 6];
+let res = reverseArray(arr);
+console.log(res);
