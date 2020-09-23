c6df9c845705929efcc41d9eb8b764c10a22f3dd
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-09-23 12:07:37 +0300

    update read me

--- __tests__/matrix-sum.test.js
+++ __tests__/matrix-sum.test.js
@@ -1,13 +1,34 @@
+
 'use strict';
 
-const multiBracketValidation = require('../challenges/multiBracketValidation/multi-bracket-validation.js');
+const matrixSum = require('../challenges/matrixSum/matrix-sum.js');
 
-console.log('multiBracketValidation', multiBracketValidation);
+console.log('matrixSum', matrixSum);
 
 describe('matrixSum Module', () => {
-  it('multiBracketValidation() give true when balanced brackets inserted', () => {
-    let input = '{}(){}';
-    expect(multiBracketValidation.multiBracketValidation(input)).toBeTruthy();
+  it('matrixSum() give a valid output', () => {
+    let matrix = [ [1, 2, 3], [3, 5, 7], [1, 7, 10] ];
+    expect(matrixSum.matrixSum(matrix)).toEqual([6, 15, 18]);
   });
 
+  it('insertShiftArray() give a valid output', () => {
+    let matrix = [ [0, 1, 5], [-4, 7, 2], [-3, 12, 11] ];
+    expect(matrixSum.matrixSum(matrix)).toEqual([6, 5, 20]);
+  });
+
+  it('should throw error if passed no arguments', () => {
+    try {
+      matrixSum.matrixSum();
+    } catch (error) {
+      expect(error).toEqual(new Error('Please insert a valid arguments'));
+    }
+  });
+
+  it('should throw error if passed invalid arguments', () => {
+    try {
+      matrixSum.matrixSum([2, 3]);
+    } catch (error) {
+      expect(error).toEqual(new Error('Please insert a valid arguments'));
+    }
+  });
 });
