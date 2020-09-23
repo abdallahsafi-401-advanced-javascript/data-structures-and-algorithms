0f7e1d0638741229b2c2b9e6b5e179168a06a35b
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-09-23 11:31:38 +0300

    solving with stacks

--- __tests__/matrix-sum.test.js
+++ __tests__/matrix-sum.test.js
@@ -1,33 +1,13 @@
 'use strict';
 
-const matrixSum = require('../challenges/matrixSum/matrix-sum.js');
+const multiBracketValidation = require('../challenges/multiBracketValidation/multi-bracket-validation.js');
 
-console.log('matrixSum', matrixSum);
+console.log('multiBracketValidation', multiBracketValidation);
 
 describe('matrixSum Module', () => {
-  it('matrixSum() give a valid output', () => {
-    let matrix = [ [1, 2, 3], [3, 5, 7], [1, 7, 10] ];
-    expect(matrixSum.matrixSum(matrix)).toEqual([6, 15, 18]);
+  it('multiBracketValidation() give true when balanced brackets inserted', () => {
+    let input = '{}(){}';
+    expect(multiBracketValidation.multiBracketValidation(input)).toBeTruthy();
   });
 
-  it('insertShiftArray() give a valid output', () => {
-    let matrix = [ [0, 1, 5], [-4, 7, 2], [-3, 12, 11] ];
-    expect(matrixSum.matrixSum(matrix)).toEqual([6, 5, 20]);
-  });
-
-  it('should throw error if passed no arguments', () => {
-    try {
-      matrixSum.matrixSum();
-    } catch (error) {
-      expect(error).toEqual(new Error('Please insert a valid arguments'));
-    }
-  });
-
-  it('should throw error if passed invalid arguments', () => {
-    try {
-      matrixSum.matrixSum([2, 3]);
-    } catch (error) {
-      expect(error).toEqual(new Error('Please insert a valid arguments'));
-    }
-  });
 });
--- challenges/multiBracketValidation/multi-bracket-validation.js
+++ challenges/multiBracketValidation/multi-bracket-validation.js
@@ -1,26 +1,56 @@
 'use strict';
+let Stack = require('../../Data-Structures/stacksAndQueues/stacks.js');
 
-function multiBracketValidation(str) {
-  const checker = [];
+//solution without stacks
+// function multiBracketValidation(str) {
+//   const checker = [];
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === '(' || str[i] === '{' || str[i] === '[') {
-      checker.push(str[i]);
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === '(' || str[i] === '{' || str[i] === '[') {
+//       checker.push(str[i]);
+//     }
+//     if (
+//       (str[i] === ')' && checker[checker.length - 1] === '(') ||
+//       (str[i] === '}' && checker[checker.length - 1] === '{') ||
+//       (str[i] === ']' && checker[checker.length - 1] === '[')
+//     ) {
+//       checker.pop();
+//     } else if (
+//       (str[i] === ')' && checker[checker.length - 1] !== '(') ||
+//       (str[i] === '}' && checker[checker.length - 1] !== '{') ||
+//       (str[i] === ']' && checker[checker.length - 1] !== '[')
+//     ) {
+//       return false;
+//     }
+//   }
+//   if (checker.length === 0) return true;
+//   return false;
+// }
+
+//solution with stacks
+
+function multiBracketValidation(input){
+  if(input==null){
+    return true;
+  }
+
+  let stack = new Stack();
+  for (let i = 0; i <input.length ; i++) {
+    let c = input.charAt(i);
+    if(c=='('){
+      stack.push(')');
     }
-    if (
-      (str[i] === ')' && checker[checker.length - 1] === '(') ||
-      (str[i] === '}' && checker[checker.length - 1] === '{') ||
-      (str[i] === ']' && checker[checker.length - 1] === '[')
-    ) {
-      checker.pop();
-    } else if (
-      (str[i] === ')' && checker[checker.length - 1] !== '(') ||
-      (str[i] === '}' && checker[checker.length - 1] !== '{') ||
-      (str[i] === ']' && checker[checker.length - 1] !== '[')
-    ) {
-      return false;
+    if(c=='['){
+      stack.push(']');
+    }
+    if(c=='{'){
+      stack.push('}');
+    }else if (c==')' || c=='}' || c==']' ){
+      if(stack.isEmpty() || stack.pop()!=c)
+        return false;
     }
   }
-  if (checker.length === 0) return true;
-  return false;
-}
+  return stack.isEmpty();
+} 
+
+module.exports.multiBracketValidation = multiBracketValidation;
