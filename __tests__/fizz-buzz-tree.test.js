d89f9e853d760554919190b06bc9549988b6fd9c
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-09-29 22:03:56 +0300

    implement fizzbuzz and test

---
+++ __tests__/fizz-buzz-tree.test.js
@@ -0,0 +1,96 @@
+'use strict';
+const fizzBuzzTree = require('../challenges/fizzBuzzTree/fizz-buzz-tree.js');
+
+describe('FizzBuzzTree Module', () => {
+  let tree;
+  beforeAll(() => {
+    class Node {
+      constructor(value = null) {
+        this.value = value;
+        this.parent = null;
+        this.children = [];
+      }
+    }
+
+    // creating tree constructor
+    class Tree {
+      constructor(value) {
+        let node = new Node(value);
+        this.root = node;
+      }
+    }
+
+    // creating 3-ary tree
+    tree = new Tree(3);
+
+    tree.root.children.push(new Node(15));
+    tree.root.children[0].parent = tree;
+
+    tree.root.children.push(new Node(10));
+    tree.root.children[1].parent = tree;
+
+    tree.root.children.push(new Node(9));
+    tree.root.children[2].parent = tree;
+
+    tree.root.children[0].children.push(new Node(4));
+    tree.root.children[0].children[0].parent = tree.root.children[0];
+
+    tree.root.children[0].children.push(new Node(20));
+    tree.root.children[0].children[1].parent = tree.root.children[0];
+  });
+
+  it('fizzBuzzTree()', () => {
+    //     console.log('tree --->',fizzBuzzTree.fizzBuzzTree(tree).tree);
+    //     console.log('FizzBuzz ----->',fizzBuzzTree.fizzBuzzTree(tree)['FizzBuzz']);
+    expect(fizzBuzzTree.fizzBuzzTree(tree)['tree']).toEqual([
+      3,
+      15,
+      4,
+      20,
+      10,
+      9,
+    ]);
+    expect(fizzBuzzTree.fizzBuzzTree(tree)['FizzBuzz']).toEqual([
+      'Fizz',
+      'FizzBuzz',
+      4,
+      'Buzz',
+      'Buzz',
+      'Fizz',
+    ]);
+    expect(fizzBuzzTree.fizzBuzzTree(tree)['tree']).toEqual([
+      'Fizz',
+      'FizzBuzz',
+      4,
+      'Buzz',
+      'Buzz',
+      'Fizz',
+    ]);
+  });
+
+  it('fizzBuzzTree() throw error if tree is empty', () => {
+    class Node {
+      constructor(value = null) {
+        this.value = value;
+        this.parent = null;
+        this.children = [];
+      }
+    }
+  
+    // creating tree constructor
+    class Tree {
+      constructor(value) {
+        let node = new Node(value);
+        this.root = node;
+      }
+    }
+  
+    // creating 3-ary tree
+    let tree = new Tree(null);
+    try {
+      fizzBuzzTree.fizzBuzzTree(tree);
+    } catch (error) {
+      expect(error).toEqual(new Error('tree is empty'));
+    }
+  });
+});
---
+++ challenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -0,0 +1,32 @@
+'use strict';
+
+function fizzBuzzTree(tree) {
+  if (tree.root == null) {
+    throw new Error('tree is empty');
+  }
+  let outputTree = [];
+  let outputFizzBuzz = [];
+  let _traverse = (node) => {
+    if (node) {
+      outputTree.push(node.value);
+      node.value % 3 === 0 && node.value % 5 === 0
+        ? (node.value = 'FizzBuzz')
+        : node.value % 3 === 0
+          ? (node.value = 'Fizz')
+          : node.value % 5 === 0
+            ? (node.value = 'Buzz')
+            : node.value;
+      outputFizzBuzz.push(node.value);
+      for (let i = 0; i < node.children.length; i++) {
+        node.children.length && _traverse(node.children[i]);
+      }
+    }
+  };
+  _traverse(tree.root);
+  return {
+    tree: outputTree,
+    FizzBuzz: outputFizzBuzz,
+  };
+}
+
+module.exports.fizzBuzzTree = fizzBuzzTree;
