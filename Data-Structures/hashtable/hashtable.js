495245033b552e151658d4911742fbae598d0862
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-10-18 15:22:31 +0300

    complelte implementation and tests

--- Data-Structures/hashtable/hashtable.js
+++ Data-Structures/hashtable/hashtable.js
@@ -3,11 +3,12 @@
 class HashTable {
   constructor(size) {
     this.size = size;
-    this.entries = new Array(size); 
+    this.entries = new Array(size);
   }
 
   hash(key) {
     let charArr = key.split('');
+    console.log('charArr ----->', charArr);
     return (
       (charArr.reduce((p, n) => {
         return p + n.charCodeAt(0);
@@ -19,6 +20,7 @@ class HashTable {
 
   add(key, value) {
     let hashIndex = this.hash(key);
+    console.log('hashIndex ----->', hashIndex);
     if (!this.entries[hashIndex]) {
       this.entries[hashIndex] = new LinkedList();
     }
@@ -28,21 +30,20 @@ class HashTable {
   }
 
   get(key) {
+    console.log('this.contains(key) ----->', this.contains(key));
     if (this.contains(key)) {
-      let value = this.entries[this.hash(key)].filter(
-        (bucket) => Object.keys(bucket)[0] === key,
-      )[0][key];
+      let value = this.entries[this.hash(key)].retrieve(key);
       return value;
     }
     return null;
   }
 
   contains(key) {
+    console.log('contain entries', this.entries[this.hash(key)]);
     return (
       this.entries[this.hash(key)] &&
-      !!this.entries[this.hash(key)].find(
-        (bucket) => Object.keys(bucket)[0] === key,
-      )
+      !!this.entries[this.hash(key)].include(key)
+      // (bucket) => Object.keys(bucket)[0] === key,
     );
   }
 }
@@ -70,6 +71,34 @@ class LinkedList {
       }
       current.next = node;
     }
+  }
+
+  // Search for a given value
+  include(key) {
+    console.log('value ---->', key);
+    let current = this.head;
+    while (current) {
+      console.log('current---->', current);
+      if (current.value[key]) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  // Search for a given value
+  retrieve(key) {
+    console.log('value ---->', key);
+    let current = this.head;
+    while (current) {
+      console.log('current---->', current);
+      if (current.value[key]) {
+        return current.value[key];
+      }
+      current = current.next;
+    }
+    return false;
   }
 }
 
