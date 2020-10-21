0cd786b2d4875514ceb358d1d1343428c70e3931
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-10-21 06:39:11 +0300

    complete

---
+++ challenges/leftJoin/left-join.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = (leftHash, rightHash) => {
+
+  if (!(rightHash && leftHash)) { throw new Error('No values in hash table(s)'); }
+
+  let outputArray = [];
+
+  for (let key in leftHash) {
+    outputArray
+      .push([key, leftHash[key], (key in rightHash ? rightHash[key] : null)]);
+  }
+
+  return outputArray;
+};
